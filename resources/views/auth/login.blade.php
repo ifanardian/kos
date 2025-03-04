399297bac2758239cb1caf3879b84ad997fd2e6f
fionamaharani <672021160@student.uksw.edu>
2025-03-04 13:36:55 +0700

    forget password

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -18,7 +18,7 @@
     <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/pannellum@2.5.6/build/pannellum.js"></script>
     <title>Login | Kos Fortuna</title>
     <style>
-        body {
+        /* body {
             font-family: "Lexend", sans-serif;
             font-optical-sizing: auto;
             font-style: normal;
@@ -61,13 +61,164 @@
             background-color: #f8d7da;
             color: #721c24;
             border: 1px solid #f5c6cb;
+        } */
+
+        body{
+            margin: 0;
+            padding: 0;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            min-height: 100vh;
+            font-family: "Lexend", sans-serif;
+            background-image: url("{{ asset('images/flipped-house.png') }}");
+            background-size: cover;
+            background-attachment: fixed;
+            background-repeat: no-repeat;
+            overflow: hidden;
+            /* background: linear-gradient(to bottom, #0f0c29, #302b63, #24243e); */
         }
+        .main{
+            width: 450px;
+            height: 500px;
+            background: #e5f4fb;
+            overflow: hidden;border-radius: 10px;
+            box-shadow: 5px 20px 50px #000;
+        }
+        #chk{
+            display: none;
+        }
+        .signup{
+            position: relative;
+            width:100%;
+            height: 100%;
+        }
+        label{
+            color: #143443;
+            font-size: 2em;
+            justify-content: center;
+            display: flex;
+            margin: 50px;
+            font-weight: bold;
+            cursor: pointer;
+            transition: .5s ease-in-out;
+        }
+        input{
+            width: 66%;
+            height: 37px;
+            background: #fff; /*#e0dede*/
+            justify-content: center;
+            display: flex;
+            margin: 30px auto;
+            padding: 19px;
+            border: none;
+            outline: none;
+            border-radius: 5px;
+        }
+        .signup button{
+            width: 40%;
+            height: 40px;
+            margin: 10px auto;
+            justify-content: center;
+            display: block;
+            color: #fff;
+            background: #174054;
+            font-size: 1em;
+            font-weight: bold;
+            margin-top: 40px;
+            outline: none;
+            border: none;
+            border-radius: 5px;
+            transition: .2s ease-in;
+            cursor: pointer;
+        }
+        button:hover{
+            background: #174054;
+        }
+        .login{
+            height: 460px;
+            background: #102d3b;
+            border-radius: 60% / 10%;
+            transform: translateY(-180px);
+            transition: .8s ease-in-out;
+        }
+        .login label{
+            /* margin: 1rem; */
+            white-space: nowrap;
+            color: #ffffff;
+            font-size: 1.9em;
+            transform: scale(.6);
+        }
+        .login button{
+            width: 50%;
+            height: 40px;
+            margin: 10px auto;
+            justify-content: center;
+            display: block;
+            color: #174054;
+            background: #E3F6FF;
+            font-size: 1em;
+            font-weight: bold;
+            margin-top: 40px;
+            outline: none;
+            border: none;
+            border-radius: 5px;
+            transition: .2s ease-in;
+            cursor: pointer;
+        }
+
+        #chk:checked ~ .login{
+            transform: translateY(-500px);
+        }
+        #chk:checked ~ .login label{
+            transform: scale(1);	
+        }
+        #chk:checked ~ .signup label{
+            padding: 80px;
+            transform: scale(.6);
+        }
+
 
     </style>
 </head>
 
 <body>
-    <section class="vh-100">
+    <div class="main">  	
+		<input type="checkbox" id="chk" aria-hidden="true">
+
+			<div class="signup">
+                @if (session('success'))
+                    <div class="alert alert-success alert-dismissible fade show" role="alert">
+                        {{ session('success') }}
+                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                    </div>
+                @endif
+
+                @if ($errors->has('error'))
+                    <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                        {{ $errors->first('error') }}
+                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                    </div>
+                @endif
+
+				<form method="POST" action="{{ route('actionlogin') }}" id="loginForm">
+                    @csrf
+					<label for="chk" aria-hidden="true">Login</label>
+					<input type="email" name="email" placeholder="Email" id="email" required="" >
+					<input type="password" name="password" placeholder="Password" id="password" required="">
+					<button type="submit" value="submit">LOGIN</button>
+				</form>
+			</div>
+
+			<div class="login">
+				<form>
+					<label for="chk" aria-hidden="true">Lupa Password</label>
+					<input type="email" name="email" placeholder="Email" required="">
+					<button>RESET PASSWORD</button>
+				</form>
+			</div>
+	</div>
+    {{-- <section class="vh-100">
         <div class="container h-100">
             <div class="row d-flex justify-content-center align-items-center h-100">
                 <div class="col-lg-10 col-xl-5">
@@ -109,15 +260,6 @@
                                                     class="form-control" />
                                             </div>
                                         </div>
-
-                                        {{-- <p class="text-center">Don't have an account? <a href="{{ url('register') }}">Sign
-                                        up</a></p> --}}
-
-                                        {{-- <div class="d-flex justify-content-center mx-4 mb-3 mb-lg-4">
-                                            <button type="submit" value="submit"
-                                                class="btn btn-primary btn-lg">Login</button>
-                                        </div> --}}
-
                                         <div class="password text-center">
                                             <button class="btn_3" type="submit" value="submit">Login</button>
                                         </div>
@@ -131,7 +273,7 @@
                 </div>
             </div>
         </div>
-    </section>
+    </section> --}}
 
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script>
