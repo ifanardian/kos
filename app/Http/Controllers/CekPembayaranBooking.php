1648e690580ba60c6c5e49a44a72a4b7ee4721fb
ifan ardiansyah <672021091@student.uksw.edu>
2025-05-07 20:49:53 +0700

    update cek booking payment (delete after 24H)

--- app/Http/Controllers/CekPembayaranBooking.php
+++ app/Http/Controllers/CekPembayaranBooking.php
@@ -17,28 +17,19 @@ class CekPembayaranBooking extends Controller
     public function cekPembayaranSekali()
     {   $result= NULL;
         $created = NULL;
-        $payment = Payment::where('status_verifikasi', NULL)->get();
+        $payment = Payment::where('status_verifikasi', NULL)->where('metode_pembayaran',NULL)->get();
         if (!$payment->isEmpty()) {
             foreach ($payment as $p){
                 $selisih = (now()->diffInMinutes($p->created_at))*-1;
-                if($selisih > 1440){
-                    DB::transaction(function () use ($p) {
+                if($selisih > 1440 ){
+                    DB::Delete(function () {
                         $penyewa = Penyewa::where('id_penyewa', $p->id_penyewa)->first();
-                        
-                        // Hapus data terkait penyewa
                         Users::where('email', $penyewa->email)->delete();
                         Payment::where('id_penyewa', $p->id_penyewa)->delete();
-                        
-                        // Update status booking
                         Booking::where('id_booking', $penyewa->id_booking)->update(['status' => 'CANCELED']);
-                        
-                        // Update status kamar
                         Kamar::where('id_kamar', $penyewa->no_kamar)->update(['status' => 'F']);
-                        
-                        // Hapus data penyewa
                         $penyewa->delete();
                     });
-                    
                     return response()->json(['result' =>'pembayaran sudah lebih dari 24 jam']);
                 }
             }
